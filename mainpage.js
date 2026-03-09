3d8748d4f4db08202e2a2da1cae2c0e0de0b41bf
fFAYSAL <faysalhossen2200@gmail.com>
2026-03-09 13:24:13 +0600

    filert interactive

--- mainpage.js
+++ mainpage.js
@@ -1,18 +1,39 @@
+
 const allFillterBtn = document.getElementById("all-btn");
 const openFillterBtn = document.getElementById("open-btn");
 const closedFillterBtn = document.getElementById("closed-btn");
 
 const cards = document.querySelectorAll(".issue-card");
 
+const buttons = [allFillterBtn, openFillterBtn, closedFillterBtn];
+
+function setActiveButton(activeBtn){
+    buttons.forEach(btn => {
+        btn.classList.remove("bg-blue-600","text-white");
+        btn.classList.add("bg-white","text-black");
+    });
+
+    activeBtn.classList.remove("bg-white","text-black");
+    activeBtn.classList.add("bg-blue-600","text-white");
+}
+
+
 // ALL
 allFillterBtn.addEventListener("click", () => {
+
+    setActiveButton(allFillterBtn);
+
     cards.forEach(card => {
         card.style.display = "block";
     });
+
 });
 
 // OPEN
 openFillterBtn.addEventListener("click", () => {
+
+    setActiveButton(openFillterBtn);
+
     cards.forEach(card => {
 
         if(card.classList.contains("border-green-500")){
@@ -23,9 +44,11 @@ openFillterBtn.addEventListener("click", () => {
 
     });
 });
-
 // CLOSED
 closedFillterBtn.addEventListener("click", () => {
+
+    setActiveButton(closedFillterBtn);
+
     cards.forEach(card => {
 
         if(card.classList.contains("border-purple-500")){
